a3748d112b9c62661978d1d6b8fe43a1570f01a4
Tommy-Smiley <136062@student.asd20.org>
2019-01-15 14:46:22 -0700

    finished stack

--- SeniorYearCodingClass/Stack/Stack/Program.cs
+++ SeniorYearCodingClass/Stack/Stack/Program.cs
@@ -10,6 +10,77 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            int choice=0;
+            stack mystack = new stack();
+
+            do
+            {
+                Console.WriteLine("**********");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Print");
+                Console.WriteLine("5. Search");
+                Console.WriteLine("6. Quit");
+                Console.WriteLine("**********");
+                choice = int.Parse(Console.ReadLine());
+
+                if(choice == 1)
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter a number to add: ");
+                    int addnum = int.Parse(Console.ReadLine());
+                    mystack.push(addnum);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (choice == 2)
+                {
+                    Console.WriteLine();
+                    int temp = mystack.pop();
+                    if(temp == -1)
+                    {
+                        Console.WriteLine("ERROR");
+                    }
+                    else
+                    {
+                        Console.WriteLine(temp);
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (choice == 3)
+                {
+                    Console.WriteLine();
+                    int temp = mystack.pop();
+                    if (temp == -1)
+                    {
+                        Console.WriteLine("ERROR");
+                    }
+                    else
+                    {
+                        Console.WriteLine(temp);
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (choice == 4)
+                {
+                    Console.WriteLine();
+                    mystack.print();
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (choice == 5)
+                { 
+                    Console.WriteLine();
+                    Console.Write("Enter a value to search for: ");
+                    int use = int.Parse(Console.ReadLine());
+                    Console.WriteLine(mystack.search(use));
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (choice != 6);
         }
     }
 }
---
+++ SeniorYearCodingClass/Stack/Stack/stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class stack
+    {
+        List<int> hold = new List<int>();
+
+        public stack()
+        {
+
+        }
+
+        public void push(int val)
+        {
+            hold.Add(val);
+        }
+
+        public int pop()
+        {
+            if (hold.Count > 0)
+            {
+                int num = 0;
+                num = hold[hold.Count - 1];
+                hold.RemoveAt(hold.Count - 1);
+                return num;
+            }
+            return -1;
+        }
+
+        public int peek()
+        {
+            if (hold.Count > 0)
+            {
+                return hold[hold.Count - 1];
+            }
+            return -1;
+        }
+
+        public int search(int val)
+        {
+            int count = 0;
+            for(int i = hold.Count - 1; i > -1; i--)
+            {
+                if (hold[i] == val)
+                {
+                    return count;
+                }
+                count++;
+            }
+            return -1;
+          
+        }
+
+        public void print()
+        {
+            for (int i = hold.Count - 1; i > -1; i--)
+            {
+                Console.WriteLine(hold[i]);
+            }
+        }
+    }
+}
