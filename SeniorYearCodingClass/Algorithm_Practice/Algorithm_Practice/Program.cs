7df0135749bea4d8929cbbbc46abd22ead2f3bab
Tommy-Smiley <136062@student.asd20.org>
2019-02-05 14:49:35 -0700

    algorith made

---
+++ SeniorYearCodingClass/Algorithm_Practice/Algorithm_Practice/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path1 = AppDomain.CurrentDomain.BaseDirectory + @"Prob01.in_.txt";
+            string path2 = AppDomain.CurrentDomain.BaseDirectory + @"Prob02.in_.txt";
+            string path3 = AppDomain.CurrentDomain.BaseDirectory + @"Prob03.in_.txt";
+
+            List<string> file = new List<string>();
+
+            using(StreamReader sr = new StreamReader(path1))
+            {
+                string line;
+
+                while((line = sr.ReadLine()) != null)
+                {
+                    file.Add(line);
+                }
+            }
+
+            for(int i = 0; i < file.Count; i++)
+            {
+                if(file[i].Contains("QUARTER"))
+                {
+                    Console.WriteLine("q");
+                }
+                if (file[i].Contains("DIME"))
+                {
+                    Console.WriteLine("d");
+                }
+                if (file[i].Contains("NICKEL"))
+                {
+                    Console.WriteLine("n");
+                }
+                if (file[i].Contains("PENNY"))
+                {
+                    Console.WriteLine("p");
+                }
+                if (file[i].Contains("HALFDOLLAR"))
+                {
+                    Console.WriteLine("hd");
+                }
+            }
+
+
+                Console.ReadKey();
+        }
+    }
+}
--- SeniorYearCodingClass/Debugging/Debugging/Program.cs
+++ SeniorYearCodingClass/Debugging/Debugging/Program.cs
@@ -58,7 +58,7 @@ namespace Debugging
         /// <param name="upperBound">The highest value to consider</param>
         static void MultiplesOfThreeAndFive(int lowerBound, int upperBound)
         {
-            for(int i = 0; i < 100; i++)
+            for(int i = lowerBound; i <= upperBound; i++)
             {
                 if (i % 3 == 0 || i % 5 == 0)
                 {
@@ -74,8 +74,8 @@ namespace Debugging
         /// <param name="sizeOfArray">Size of array</param>
         static void CreateArrayOfRandomIntegers(int sizeOfArray)
         {
-            int[] myArray = new int[10];
-            for(int i = 0; i <= sizeOfArray; i++)
+            int[] myArray = new int[sizeOfArray];
+            for(int i = 0; i <= sizeOfArray-1; i++)
             {
                 myArray[i] = rand.Next(0, 1000);
             }
@@ -95,7 +95,7 @@ namespace Debugging
             //formula = (fahrenheit°F − 32) × (5/9) = celsius°C
             //use google to verify your input
             Console.Write(fahrenheit + " degrees Fahrenheit is ");
-            float modifer = (5 / 9);
+            float modifer = 5 / 9.0f;
             float celsius = (fahrenheit - 32) * modifer;
             Console.WriteLine(celsius + " degrees celsius.");
         }
