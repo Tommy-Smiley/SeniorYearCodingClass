ddc12c5ecd65b8c80c73891f53aa92e48be8247f
Tommy-Smiley <136062@student.asd20.org>
2019-02-13 14:52:56 -0700

    cyber problems

---
+++ SeniorYearCodingClass/CyberBoizProblem_1/CyberBoizProblem_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CyberBoizProblem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> file = new List<string>();
+            List<int> nums1 = new List<int>();
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"Prob01.in.txt";
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                        file.Add(line);
+                }
+
+            }
+            nums1[0] = int.Parse(file[0].Substring(0, 1));
+            
+
+            //print
+            for (int i = 0; i < file.Count; i++)
+            {
+                Console.WriteLine(file[i]);
+            }
+            Console.WriteLine(nums1[0]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SeniorYearCodingClass/CyberboizProblem_2/CyberboizProblem_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CyberboizProblem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"prob02.in.txt";
+            string line;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (line=sr.ReadLine() != null)
+            }
+
+
+
+        }
+    }
+}
