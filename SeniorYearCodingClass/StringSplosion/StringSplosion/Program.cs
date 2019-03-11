8a6a09cd505b55e2a59a9c9214b8955e7d820149
Tommy-Smiley <136062@student.asd20.org>
2019-03-11 12:29:36 -0600

    string quiz

---
+++ SeniorYearCodingClass/StringSplosion/StringSplosion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Code";
+            Console.WriteLine(result(test));
+            Console.ReadKey();
+        }
+        static string result(string word)
+        {
+            string end = "";
+
+            for (int i = 0; i < word.Length + 1; i++)
+            {
+                end += word.Substring(0, i);
+            }
+            return end;
+        }
+    }
+}
