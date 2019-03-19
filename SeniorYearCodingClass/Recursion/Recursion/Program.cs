8b6c9cbe0d37a63dd5cf4077526a816a992430a5
Tommy-Smiley <136062@student.asd20.org>
2019-03-19 14:47:26 -0600

    Recursion practice

---
+++ SeniorYearCodingClass/Recursion/Recursion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 17;
+
+            Console.WriteLine(CheckforPrime(test));
+            Console.ReadKey();
+        }
+
+        static bool CheckforPrime(int n, int divisor = 2)
+        {
+            if (n % divisor == 0)
+            {
+                return false;
+            }
+            if (divisor == n - 1)
+            {
+                return true;
+            }
+
+            return CheckforPrime(n, divisor + 1);
+        }
+    }
+}
