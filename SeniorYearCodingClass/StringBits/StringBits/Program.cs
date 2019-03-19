641a33876d6930e032358329a8d318ec51dcb3d5
Tommy-Smiley <136062@student.asd20.org>
2019-03-19 12:10:58 -0600

    string bits quiz

---
+++ SeniorYearCodingClass/StringBits/StringBits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello";
+            Console.WriteLine(bits(word));
+            Console.ReadKey();
+        }
+
+        static string bits(string wurd)
+        {
+            string end = "";
+
+            for (int i = 0; i < wurd.Length; i+=2)
+            {
+                end += wurd[i];
+            }
+            return end;
+        }
+    }
+}
