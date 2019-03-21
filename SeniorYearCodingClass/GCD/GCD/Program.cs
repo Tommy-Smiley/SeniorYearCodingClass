01ade9e856750dcc9c61a441456c5cc210ed4f47
Tommy-Smiley <136062@student.asd20.org>
2019-03-21 14:46:27 -0600

    made GCD and did something to Recursion

---
+++ SeniorYearCodingClass/GCD/GCD/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(gcd(num1,num2));
+            Console.ReadKey();
+        }
+
+        static int gcd(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+
+            else if (b == 0)
+            {
+                return a;
+            }
+
+            else
+            {
+                return gcd(b, a % b);
+            }
+
+        }
+
+    }
+}
